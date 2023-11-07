2eaacbf72ab414e740c7d296ee21708155b2593a
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-07 11:10:11 +0300

    page routes

---
+++ app/reviews/page.jsx
@@ -0,0 +1,10 @@
+export default function ReviewsPage() {
+   return (
+      <>
+         <h1>Reviews Page</h1>
+         <p>
+            Here we'll list all the reviews   
+         </p>      
+      </>
+   );
+}
