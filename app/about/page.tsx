a3dde910ae95beb35cebd5b40c853a9d7569718c
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-08 11:16:05 +0300

    extra pages

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+   return (
+      <>
+         <h1>About Us</h1>
+         <p>
+            We are the top Indie Game review site in the business!
+         </p>      
+      </>
+   );
+}
---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,10 @@
+export default function HollowKnight() {
+   return (
+      <>
+         <h1>Hollow Knight Revies</h1>
+         <p>
+            this the one and only best game in its category.
+         </p>      
+      </>
+   );
+}
