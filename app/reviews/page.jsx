25911f102d4fa810e839ce146d2d0e0d1fcc4790
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-11 16:00:56 +0300

    review links listing

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function ReviewsPage() {
    return (
       <>
          <h1>Reviews Page</h1>
          <p>
-            Here we'll list all the reviews   
-         </p>      
+            Here we'll list all the reviews:
+         </p>
+         
+            <ol>
+               <li><Link href="/reviews/stardew-valley">StarDew Valley</Link></li>
+               <li><Link href="/reviews/hollow-knight">Hollow Knight</Link></li>
+            </ol>
+         
       </>
    );
 }
