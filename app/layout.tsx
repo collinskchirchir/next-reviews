e5058ef82b726f86234b975b547c7c280f8ae0a1
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-13 17:11:39 +0300

    layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,20 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
    return (
       <html lang="en">
-         <body>
+         <body className="flex flex-col px-8 py-2 min-h-screen">
             <header>
                <nav>
-                  <ul>
+                  <ul className="flex gap-2">
                      <li><Link href="/">Home</Link></li>
                      <li><Link href="/reviews">Reviews</Link></li>
                      <li><Link prefetch={false} href="/about">About</Link></li>                   
                   </ul>
                </nav>
             </header>
-            <main>
+            <main className="grow py-3">
                {children}
             </main>
-            <footer>
+            <footer className="text-center text-xs border-t py-3">
                Game data and images courtesy
                 of <a href="https://rawg.io">RAWG</a>
             </footer>
