f7a05287394330b7e10016f7b613ca69e98e368d
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-13 17:27:33 +0300

    color pallet styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import Link from "next/link";
+import NavBar from "../components/NavBar";
 import './globals.css'
 
 interface LayoutProps {
@@ -9,22 +9,20 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
    return (
       <html lang="en">
-         <body className="flex flex-col px-8 py-2 min-h-screen">
+         <body className="bg-orange-50 flex flex-col px-8 py-2 min-h-screen">
             <header>
-               <nav>
-                  <ul className="flex gap-2">
-                     <li><Link href="/">Home</Link></li>
-                     <li><Link href="/reviews">Reviews</Link></li>
-                     <li><Link prefetch={false} href="/about">About</Link></li>                   
-                  </ul>
-               </nav>
+               <NavBar />
             </header>
             <main className="grow py-3">
                {children}
             </main>
             <footer className="text-center text-xs border-t py-3">
                Game data and images courtesy
-                of <a href="https://rawg.io">RAWG</a>
+                of <a 
+                href="https://rawg.io"
+                target="_blank"
+                className="text-orange-800 hover:underline"
+                >RAWG</a>
             </footer>
          </body>
       </html>
