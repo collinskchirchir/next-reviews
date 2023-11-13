42ab7000f8e7ad631e894c26a24a2d349aa0208d
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-13 21:45:23 +0300

    rendering markdown

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,14 +1,14 @@
 import { readFile } from "fs/promises";
+import { marked } from "marked";
 import Heading from "@/components/Heading";
 
 export default async function StardewValley() {
-   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8')
+   const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
+   const html = marked(text)
    return (
       <>
          <Heading>Stardew Valley</Heading>
-         <p>
-            {text}
-         </p>      
+         <article dangerouslySetInnerHTML={{__html: html}} />     
       </>
    );
 }
