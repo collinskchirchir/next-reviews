75cccc160779a855a7c96b77226bc9d37769a811
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-15 20:55:43 +0300

    front matter

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,13 +1,17 @@
 import { readFile } from "fs/promises";
+import matter from "gray-matter";
 import { marked } from "marked";
 import Heading from "@/components/Heading";
 
 export default async function StardewValley() {
    const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
-   const html = marked(text)
+   const {content, data: {title, date}} = matter(text)
+   const html = marked(content)
+   console.log(title)
    return (
       <>
-         <Heading>Stardew Valley</Heading>
+         <Heading>{title}</Heading>
+         {date}
          <article 
             dangerouslySetInnerHTML={{__html: html}}
             className="prose prose-slate"         
--- content/reviews/stardew-valley.md
+++ content/reviews/stardew-valley.md
@@ -1,3 +1,9 @@
+---
+title: "Stardew Valley"
+date: "2023-05-04"
+image: "/images/stardew-valley.jpg"
+---
+
 ## Sub-Heading
 
 Rich text with __bold__ or _italic_
