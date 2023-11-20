0e1c99673ed3058499eaa2a0b7292c4a5765dd73
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-20 12:02:08 +0300

    Link icon

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { LinkIcon } from '@heroicons/react/20/solid'
+
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -14,9 +16,10 @@ export default function ShareLinkButton() {
    return (
       <button
          onClick={handleClick}
-         className="border px-2 py-1 rounded text-slate-500 text-sm
+         className="border flex gap-1 items-center px-2 py-1 rounded text-slate-500 text-sm
             hover:bg-orange-100 hover:text-slate-700 hover:cursor-pointer"
       >
+         <LinkIcon className='h-4 w-4' />
          {clicked ? 'Link Copied': 'Share Link'}
       </button>
    )
