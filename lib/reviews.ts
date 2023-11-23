95611fac43352267c103b3974166c8ad5a9838b7
Collins Chirchir <collinskchirchir@gmail.com>
2023-11-23 14:46:12 +0300

    get reviews from CMS

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -2,6 +2,7 @@ import exp from "constants";
 import { readdir, readFile } from "fs/promises";
 import matter from "gray-matter";
 import { marked } from "marked";
+import qs from 'qs';
 
 export interface Review {
    title: string;
@@ -13,32 +14,43 @@ export interface Review {
 
 export async function getReview(slug: string): Promise<Review> {
    const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
-   const {content, data: {title, date, image}} = matter(text)
+   const { content, data: { title, date, image } } = matter(text)
    const body = marked(content)
-   return { slug, title, date, image, body}
+   return { slug, title, date, image, body }
 }
 
-export async function getReviews(): Promise<Review[]> { 
-   const slugs = await getSlugs();
-   // declare empty array
-   const reviews = [];
-   for (const slug of slugs) {
-      const review = await getReview(slug);
-      reviews.push(review)
-   }
-   // sort Date descending order
-   reviews.sort((a, b) => b.date.localeCompare(a.date));
-   return reviews;
+/* 
+   slug: 'hollow-knight',
+    title: 'Hollow Knight',
+    date: '2013-10-25',
+    image: '/images/hollow-knight.jpg',
+*/
+
+export async function getReviews(): Promise<Review[]> {
+   const url = 'http://localhost:1337/api/reviews?'
+         + qs.stringify({
+         fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+         populate: { image: { fields: ['url'] } },
+         pagination: { pageSize: 6 },
+         sort: ['publishedAt:desc']
+      }, { encodeValuesOnly: true });
+   console.log('getReviews: ', url)
+   const response = await fetch(url);
+   const { data } = await response.json();
+   return data.map(({ attributes }) => ({
+      slug: attributes.slug,
+      title: attributes.title,
+   }));
 }
 
 export async function getSlugs(): Promise<string[]> {
-   const files = await readdir('./content/reviews'); 
+   const files = await readdir('./content/reviews');
    return files.filter((file) => file.endsWith('.md'))
       .map(file => file.slice(0, -'.md'.length))
 }
 
 // get Featured Review
- export async function getFeaturedReview(): Promise<Review> {
+export async function getFeaturedReview(): Promise<Review> {
    const reviews = await getReviews();
    return reviews[0];
- }
+}
